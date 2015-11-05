a920cf4664869d4d63629af541cb7662f0cc7824
Ahmad <a.i.abdelghany@gmail.com>
2015-11-05 13:28:45 +0000

    add more inline comments

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
+// configure error reporting to view all errors (should be changed in production mode)
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
+// import dependencies
 require_once "lib/Smarty.class.php";
 require_once "database.php";
 
 //connect to our db
 $db = new Db();
 
+// set activeNoteId to the value from the cookie if possible otherwise set it to latest note id from db
 if(isset($_COOKIE['ACTIVE_NOTE_ID']) && $db->isValid($_COOKIE['ACTIVE_NOTE_ID'])) {
     $activeNoteId = $_COOKIE['ACTIVE_NOTE_ID'];
 } else {
@@ -15,6 +18,7 @@ if(isset($_COOKIE['ACTIVE_NOTE_ID']) && $db->isValid($_COOKIE['ACTIVE_NOTE_ID'])
     $activeNoteId = $db->getMaxId();
 }
 
+// check the desired action and act accordingly 
 if(isset($_REQUEST['action'])) {
     switch($_REQUEST['action']) {
         case 'delete':
@@ -39,8 +43,10 @@ if(isset($_REQUEST['action'])) {
     }
 }
 
+// create Smarty template
 $template = new Smarty();
 
+// pass the variables to the corresponding template and display it
 $template->assign("ACTIVE_NOTE_ID", $activeNoteId);
 $template->assign("notes", $db->getNotes());
 $template->display('index.tpl');
